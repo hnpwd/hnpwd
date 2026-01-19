b31f8db8568572beb7c8a780b115c9d5f555024b
Murat Koptur <mkoptur3@gmail.com>
2026-01-19 19:37:22 +0300

    Add user profile for Murat Koptur

--- pwd.lisp
+++ pwd.lisp
@@ -559,6 +559,12 @@
   :hnuid "mkasberg"
   :bio "Husband.  Father.  Software engineer.  Ubuntu Linux user.")
 
+ (:name "Murat Koptur"
+  :site "https://muratkoptur.com/"
+  :blog "https://muratkoptur.com/publications"
+  :hnuid "mrtkp9993"
+  :bio "Data scientist and software engineer.")
+ 
  (:name "Nabraj"
   :site "https://nabraj.com/"
   :blog "https://nabraj.com/blog"
